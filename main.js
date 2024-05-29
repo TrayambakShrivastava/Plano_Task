eae7b5e95cc52dc1452ddeaee1c61203d3e5defc
TrayambakShrivastava <99034327+TrayambakShrivastava@users.noreply.github.com>
2024-05-29 12:08:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+function removeTop(){
+    var x = document.getElementById("topMenu");
+    x.style.display = "none" ;
+}
+
+
+function shiftImg(n) {
+    var x = document.getElementsByClassName("imgSlide");
+    var y = document.getElementsByClassName("dot");
+    var currentIndex;
+    // Find the currently displayed image
+    for (var i = 0; i < x.length; i++) {
+        if (x[i].style.display === "flex") {
+            currentIndex = i;
+            break;
+        }
+    }
+    x[currentIndex].style.display = "none";
+    y[currentIndex].style.backgroundColor = "gray";
+    var nextIndex = (currentIndex + n + x.length) % x.length;
+    x[nextIndex].style.display = "flex";
+    y[nextIndex].style.backgroundColor = "blue";
+
+}
+
+
+function showIcon(index)
+{
+    var x = document.getElementsByClassName("tstmnlIcon") ;
+    var y = document.getElementsByClassName("logoDot") ;
+    var z = document.getElementsByClassName("tstmnlText") ;
+
+    for (var i = 0; i < x.length; i++) {
+        x[i].style.fill = "black" ;
+        y[i].style.backgroundColor = "gray" ;
+        z[i].style.display = "none" ;
+        }
+
+        x[index].style.fill = "aqua" ;
+        y[index].style.backgroundColor = "aqua" ;
+        z[index].style.display = "block" ;
+    
+}
+
